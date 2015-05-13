364cdcbfc725a25d769ccff48abc91aa6e101bdf
Formid <formid1990@gmail.com>
2015-05-14 00:36:58 +0800

    test

--- Matlab Functions/Get_Mat.m
+++
@@ -1,9 +0,0 @@
-function mat = Get_Mat(file_dir, size, type)
-
-file = fopen(file_dir, 'rb');
-mat = zeros(size(1), size(2), type);
-for i = 1:size(1)
-    mat(i,:) = fread(file, size(2), type);
-end
-fclose(file);
-end
